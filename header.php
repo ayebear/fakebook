6873fe4a9c8c1eb3415bc7cc0b20a7fd13eaea49
epicwinrarftw <john.ryan@rutgers.edu>
2014-05-11 01:09:30 -0400

    organized things a bit

--- header.php
+++ header.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" type="text/css" href="static/css/style.css">
 </head>
 <body>
     <div id = "banner">
 	<a href = "index.php">
-	    <img src = "res/img/banner.png" alt = "Fakebook Banner (banner.png)">
+	    <img src = "static/img/banner.png" alt = "Fakebook Banner (banner.png)">
 	</a>
     </div>
 </body>
